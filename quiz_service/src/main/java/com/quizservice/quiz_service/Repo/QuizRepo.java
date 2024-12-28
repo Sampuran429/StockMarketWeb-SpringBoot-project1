090ca4302a1cf898e8d6b29ebb2877b214e215ad
Sampuran429 <143642553+Sampuran429@users.noreply.github.com>
2024-12-28 21:18:14 +0530

    Removed folders from GitHub repo2, kept them locally

--- quiz_service/src/main/java/com/quizservice/quiz_service/Repo/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package com.quizservice.quiz_service.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quizservice.quiz_service.Model.Quiz;
-
-public interface QuizRepo extends JpaRepository<Quiz,Integer> {
-    
-}
